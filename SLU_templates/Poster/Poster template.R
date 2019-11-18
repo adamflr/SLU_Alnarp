0e0bc2415d1f05179044110aabb407b98604d734
Adam Flöhr <adam.flohr@slu.se>
2019-11-18 14:37:28 +0100

    Poster init.

---
+++ SLU_templates/Poster/Poster template.R
@@ -0,0 +1,12 @@
+text <- list("Interesting headlines",
+             "Subheadings that further explain the report",
+             "Conclusion",
+             "Hit evelit, as il earupta tem-quodicit aruptatatin consedio eum lacest, occatusamusa am. Imus, ium quas aciist di destio              optat. It ent plis aut utem quo.",
+             "Subheading",
+             "Quis atur, sum quo et volorum aut dolorio consequi qui beaturi orpore est, expe prehentur quat mo veliquae dolorecate pa volup-tust?",
+             "Subheading",
+             "Ad el moloreperum nonsequia ilis ma alit, sequidebis seque re-risquibus, ne re pa debis au.\n Et ut optae. Piscitatis cus adisita tectem int rerferia poritam ad ma-gnistio. Conse quia quam, cusci iderferum aut accum, vent.",
+             "Results",
+             "Et ut optae. Piscitatis cus adisita tectem int rerferia poritam ad ma-gnistio. Conse quia quam, cusci iderferum aut accum, vent",
+             "Us voloribus dolesti alisitiumet. Tis reperchit, es est, consequ-am qui aliquia cores volorepudit voluptatqui conse volupieniam abore disto ex eostibus a debitae pudaepu dantoreseque eos sun. Aut optas excea quatios eum sum reribus qui oditasp erunti qui iur, ipic te voloratio. Et ut optae. Piscitatis cus adisita tectem int rerferia poritam ad. Magnistio. Conse quia quam, cusci iderferum aut accum, vent. Us voloribus dolesti alisitiumet.")
+
