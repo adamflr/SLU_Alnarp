3a112bf9b9670be6862332ccd657f36eb1ffee9a
Adam Flöhr <adam.flohr@slu.se>
2020-01-09 08:30:41 +0100

    Final introductory R presentation

--- Presentations/Introduction to R/Examples.R
+++ Presentations/Introduction to R/Examples.R
@@ -5,19 +5,23 @@ dat <- Animals # Renames to dat
 library(dplyr)
 
 dat %>% 
-  mutate(brain = brain / 1000,
-<<<<<<< HEAD
-         brain_to_body_ratio = brain / body,
+  mutate(animal = row.names(dat),
+         body = body * 1000, 
          greater_than_1500 = body > 1500) -> dat
 
 library(ggplot2)
 
-g <- ggplot(dat, aes(log(brain), log(body), color = greater_than_1500)) + 
+g <- ggplot(dat, aes(log(brain), log(body), color = greater_than_1500, text = row.names(dat))) + 
   geom_point() +
   geom_smooth(method = "lm", alpha = 0.2)
 g
+
+g <- ggplot(dat, aes(log(brain), log(body), color = greater_than_1500, text = animal)) + 
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.2) +
+  theme_bw()
+g
+
 library(plotly)
-htmlwidgets::saveWidget(ggplotly(g), "Figurer/graph1.html")
-=======
-         brain_to_body_ratio = brain / body)
->>>>>>> ec5eaddab2ce6e7fa3c29ff9ca3b8ee807a432f7
+library(htmlwidgets)
+saveWidget(ggplotly(g), "c:/Eg/R/SLU_Alnarp/Presentations/Introduction to R/Figurer/graph1.html")
